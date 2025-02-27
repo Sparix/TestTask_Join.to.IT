d5b9a073731dd805d5281a1e037eee2760ed1baf
Sparix <sanya221138@gmail.com>
2025-02-27 16:22:21 +0200

    create event models, and models join to event

--- EvenAPI/models.py
+++ EvenAPI/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    organizer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    count_place = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
+class JoinEvent(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    registered_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'event')
+
+    def __str__(self):
+        return f"User: {self.user.username}, Event: {self.event.title}"
